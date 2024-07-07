b63a721c361fa738aa1671c2ea38d0fb72323a9a
sajad0day <102028361+sajad0day@users.noreply.github.com>
2024-07-07 14:31:49 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
